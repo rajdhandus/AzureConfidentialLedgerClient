d0fe1fa29b9c810d0dd6e454568aa7249bc3052c
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2022-03-31 03:13:19 -0400

    Add files via upload

---
+++ certificateBasedAuthForACL.py
@@ -0,0 +1,31 @@
+from azure.confidentialledger.identity_service import ConfidentialLedgerIdentityServiceClient
+import requests
+
+ledger_name = "test-saep"
+ledger_url = "https://" + ledger_name + ".confidential-ledger.azure.com"
+identity_url = "https://identity.confidential-ledger.core.azure.com"
+
+
+identity_client = ConfidentialLedgerIdentityServiceClient(identity_url)
+network_identity = identity_client.get_ledger_identity(
+     ledger_id=ledger_name
+)
+
+ledger_tls_cert_file_name = "networkcert.pem"
+with open(ledger_tls_cert_file_name, "w") as cert_file:
+    cert_file.write(network_identity.ledger_tls_certificate)
+    
+    
+session = requests.Session()
+
+response = session.post(
+                "https://test-saep.confidential-ledger.azure.com/app/transactions?api-version=0.1-preview",
+                verify="C:/Users/rapurush/OneDrive - Microsoft/Desktop/networkcert.pem",
+                cert=(
+                    "C:/Users/rapurush/cert.pem",
+                    "C:/Users/rapurush/privkey_name.pem",
+                ),
+                json={"contents": "hello"},
+            )
+
+print(f"result is {response.text}")
