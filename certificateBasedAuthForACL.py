7e8fadef9f65c3f8205f449af4401bb76c5d40e7
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2022-03-31 00:20:49 -0700

    Update certificateBasedAuthForACL.py

--- certificateBasedAuthForACL.py
+++ certificateBasedAuthForACL.py
@@ -21,6 +21,10 @@ session = requests.Session()
 response = session.post(
                 "https://REPLACE_ME.confidential-ledger.azure.com/app/transactions?api-version=0.1-preview",
                 verify="networkcert.pem",
+     
+#openssl ecparam -out "privkey_name.pem" -name "secp384r1" -genkey # command to produce private key
+#openssl req -new -key "privkey_name.pem" -x509 -nodes -days 365 -out "cert.pem" -"sha384" -subj=/CN="ACL Client Cert" # command to produce the corresponding Public key
+# provide private key and public key from above commands;
                 cert=(
                     "C:/REPLACE_ME/cert.pem",
                     "C:/REPLACE_ME/privkey_name.pem",
